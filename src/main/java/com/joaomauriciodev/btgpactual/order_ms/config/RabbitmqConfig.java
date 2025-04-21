cfa2cf268c6b8ed2597b246c2298685cdc2ca8e7
João Mauricio <joaomauricio.dev@gmail.com>
2025-04-20 21:27:36 -0300

    fix: add declarable queue

--- src/main/java/com/joaomauriciodev/btgpactual/order_ms/config/RabbitmqConfig.java
+++ src/main/java/com/joaomauriciodev/btgpactual/order_ms/config/RabbitmqConfig.java
@@ -1,5 +1,7 @@
 package com.joaomauriciodev.btgpactual.order_ms.config;
 
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +14,10 @@ public class RabbitmqConfig {
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Declarable orderCreatedQueue(){
+        return new Queue(ORDER_CREATED_QUEUE);
     }
 }
--- src/main/java/com/joaomauriciodev/btgpactual/order_ms/listener/OrderCreatedListener.java
+++ src/main/java/com/joaomauriciodev/btgpactual/order_ms/listener/OrderCreatedListener.java
@@ -1,6 +1,8 @@
 package com.joaomauriciodev.btgpactual.order_ms.listener;
 
 import com.joaomauriciodev.btgpactual.order_ms.dto.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
@@ -10,8 +12,11 @@ import static com.joaomauriciodev.btgpactual.order_ms.config.RabbitmqConfig.ORDE
 @Component
 public class OrderCreatedListener {
 
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message){
+       logger.info("Message consumed: {}", message);
 
     }
 }
