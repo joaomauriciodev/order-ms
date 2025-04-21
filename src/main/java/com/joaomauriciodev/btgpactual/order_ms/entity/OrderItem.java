78c2b750b6b6b86f2682d11544461cd5437cdece
João Mauricio <joaomauricio.dev@gmail.com>
2025-04-20 21:03:52 -0300

    feat: create order entity

---
+++ src/main/java/com/joaomauriciodev/btgpactual/order_ms/entity/OrderItem.java
@@ -0,0 +1,19 @@
+package com.joaomauriciodev.btgpactual.order_ms.entity;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+    private String product;
+    private Integer quantity;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal price;
+
+    public OrderItem(){
+
+    }
+}
