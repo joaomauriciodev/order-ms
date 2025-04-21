b89307641d6a901093d3c132094a8ec8350eec8e
João Mauricio <joaomauricio.dev@gmail.com>
2025-04-20 21:42:43 -0300

    feat: add service order on listener

--- src/main/java/com/joaomauriciodev/btgpactual/order_ms/listener/OrderCreatedListener.java
+++ src/main/java/com/joaomauriciodev/btgpactual/order_ms/listener/OrderCreatedListener.java
@@ -1,6 +1,7 @@
 package com.joaomauriciodev.btgpactual.order_ms.listener;
 
 import com.joaomauriciodev.btgpactual.order_ms.dto.OrderCreatedEvent;
+import com.joaomauriciodev.btgpactual.order_ms.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,11 +13,18 @@ import static com.joaomauriciodev.btgpactual.order_ms.config.RabbitmqConfig.ORDE
 @Component
 public class OrderCreatedListener {
 
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService){
+        this.orderService = orderService;
+    }
+
     private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
 
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message){
        logger.info("Message consumed: {}", message);
+       orderService.save(message.getPayload());
 
     }
 }
